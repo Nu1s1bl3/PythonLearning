362e943908c37b18028a5154b710334f412a5dc1
Nu1s1bl3 <g.bleyra@campusber.fr>
2025-02-07 11:54:23 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+#coding: utf-8
+
+"""
+Petit programme permettant de jouer à pile ou face
+"""
+import os
+import random
+
+entree = ""
+while(entree.lower() != "stop"):
+  piece = random.randint(0, 1)
+  entree = input("Pile ou face ? ('stop' pour arrêter) : ")
+  if (entree.lower() == "pile") and (piece == 1):
+    print("C'est gagné !")
+  elif (entree.lower() == "face") and (piece == 0):
+    print("C'est gagné !")
+  else:
+    print("C'est perdu !")
+    
